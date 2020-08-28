0da2b88256ec1e433bea063734d7b944aa888478
wuwenbang <wuwenbang@foxmail.com>
2020-08-28 22:47:30 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
+import { Button, Input } from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+import Antd from 'ant-design-vue'
+Vue.use(Antd);
+Vue.component("a-button", Button);
+Vue.component("a-input", Input);
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
